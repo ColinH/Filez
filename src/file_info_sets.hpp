d790de98b12845754be08968a06b8380d5483505
Dr. Colin Hirsch <github@colin-hirsch.net>
2023-01-08 22:05:57 +0100

    Work around GCC 12 build issue.

--- src/file_info_sets.hpp
+++ src/file_info_sets.hpp
@@ -12,8 +12,17 @@
 
 namespace filez
 {
-   using file_info_by_path_set = std::set< std::shared_ptr< file_info >, decltype( []( const auto& l, const auto& r ){ return l->path() < r->path(); } ) >;
-   using file_info_by_node_set = std::set< std::shared_ptr< file_info >, decltype( []( const auto& l, const auto& r ){ return l->stat().node() < r->stat().node(); } ) >;
+   struct file_info_by_path_less
+   {
+      [[nodiscard]] bool operator()( const std::unique_ptr< file_info >& l, const std::unique_ptr< file_info >& r ) const noexcept
+      {
+         return l->path() < r->path();
+      }
+   };
+
+   using file_info_by_path_set = std::set< std::unique_ptr< file_info >, file_info_by_path_less >;
+
+   // GCC 12 doesn't like this: using file_info_by_path_set = std::set< std::unique_ptr< file_info >, decltype( []( const auto& l, const auto& r ){ return l->path() < r->path(); } ) >;
 
    template< typename S, typename I >
    [[nodiscard]] S make_file_info_set_impl( const std::filesystem::path& path )
@@ -21,7 +30,7 @@ namespace filez
       S result;
 
       for( const auto& de : I( path ) ) {
-         if( !result.emplace( std::make_shared< file_info >( de.path() ) ).second ) {
+         if( !result.emplace( std::make_unique< file_info >( de.path() ) ).second ) {
             FILEZ_ERROR( "duplicate file set entry " << de.path() );
          }
       }
@@ -36,16 +45,6 @@ namespace filez
    [[nodiscard]] inline file_info_by_path_set make_full_file_info_by_path_set( const std::filesystem::path& path )
    {
       return make_file_info_set_impl< file_info_by_path_set, std::filesystem::recursive_directory_iterator >( path );
-   }
-
-   [[nodiscard]] inline file_info_by_node_set make_file_info_by_node_set( const std::filesystem::path& path )
-   {
-      return make_file_info_set_impl< file_info_by_node_set, std::filesystem::directory_iterator >( path );
-   }
-
-   [[nodiscard]] inline file_info_by_node_set make_full_file_info_by_node_set( const std::filesystem::path& path )
-   {
-      return make_file_info_set_impl< file_info_by_node_set, std::filesystem::recursive_directory_iterator >( path );
    }
 
 }  // namespace filez
